60852217128609536ef99e535469773d2ba71fcc
Cory <courtneynholley@gmail.com>
2021-02-02 15:55:15 -0600

    Feat: Added UserDetailsLoader in services

---
+++ src/main/java/com/codeup/facebookclone/services/UserDetailsLoader.java
@@ -0,0 +1,28 @@
+package com.codeup.facebookclone.services;
+
+import com.codeup.facebookclone.models.User;
+import com.codeup.facebookclone.models.UserWithRoles;
+import com.codeup.facebookclone.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository usersDao;
+
+    public UserDetailsLoader(UserRepository usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = usersDao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
