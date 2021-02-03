25bf49e781cd3cbd647d9830e882e5cd58429a6f
Cory <courtneynholley@gmail.com>
2021-02-03 11:57:48 -0600

    Feat:Index/Login page centered, blurred, background

--- src/main/java/com/codeup/facebookclone/controllers/IndexController.java
+++ src/main/java/com/codeup/facebookclone/controllers/IndexController.java
@@ -1,8 +1,16 @@
 package com.codeup.facebookclone.controllers;
 
+import com.codeup.facebookclone.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @GetMapping("/index")
+    public String indexPage(Model model) {
 
+        return "index";
+    }
 }
